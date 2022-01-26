b0908aaa0338df227ef0c15d2b266bcbfa6a1f34
cristian davila <cristiand395@gmail.com>
2022-01-26 22:36:12 +0100

    creating project

---
+++ src/homepage.component.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+export const HomePage = () => (
+    <div className="homepage">
+        <div className="directory-menu">
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">Home</h1>
+                    <span className="subtitle"></span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">Home</h1>
+                    <span className="subtitle"></span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">Home</h1>
+                    <span className="subtitle"></span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">Home</h1>
+                    <span className="subtitle"></span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">Home</h1>
+                    <span className="subtitle"></span>
+                </div>
+            </div>
+        </div>
+    </div>
+)
